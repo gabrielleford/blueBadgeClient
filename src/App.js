d3fc87254a23aa5c9f6d8140c8bee15c001b05aa
Katie Sallows <ktsallows@gmail.com>
2021-11-26 10:05:20 -0500

    updated packages to include react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
+import Navbar from './components/Navbar'
+import LoginSignup from './components/LoginSignup'
+import Landing from './components/Landing'
+
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <div className="App">
-      InstaPet
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
