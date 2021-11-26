661a22418bf9e56861f4848ea1ef40c8c4125a7e
Katie Sallows <ktsallows@gmail.com>
2021-11-26 10:04:13 -0500

    added some routing and login components

---
+++ src/components/LoginSignup.js
@@ -0,0 +1,13 @@
+const LoginSignup = (props) => {
+
+    return (
+        <>
+            <form>
+                <input type='email' name='email'></input>
+                <input type='password' name='password'></input>
+            </form>
+        </>
+    )
+}
+
+export default LoginSignup;
