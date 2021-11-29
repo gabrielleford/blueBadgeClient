f2a3aa04812d2b570ebe824ad4477d87a0cfd50a
Katie Sallows <ktsallows@gmail.com>
2021-11-28 19:18:46 -0500

    fetching in PostDisplay.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,31 +2,13 @@ import { useEffect, useState } from "react";
 import PostDisplay from "./PostDisplay";
 
 const Landing = (props) => {
-    const [ posts, setPosts ] = useState([]);
-
-    const fetchPublicPosts = () => {
-        fetch("http://localhost:3000/post/", {
-            method: "GET",
-            headers: new Headers({
-                "Content-Type": "application/json"
-            })
-        })
-        .then((res) => res.json())
-        .then((json) => {
-            console.log(json);
-            setPosts(json);
-        })
-    };
-
-    useEffect(() => {
-        fetchPublicPosts();
-    }, []);
+    const [getWhat, setGetWhat] = useState({ what: 'all', tag: null });
 
     return (
         <div id='landing'>
             <h5>Landing</h5>
             <p>Landing Page</p>
-            <PostDisplay posts={posts}/>
+            <PostDisplay getWhat={getWhat} />
         </div>
     )
 }
--- src/components/PostDisplay.js
+++ src/components/PostDisplay.js
@@ -1,9 +1,36 @@
 import { useState, useEffect } from 'react'
 
 const PostDisplay = (props) => {
+    const [posts, setPosts] = useState([]);
+
     const postMapper = () => {
-        return props.posts.map((post, index) => {
-            return(
+        let fetchURL;
+        switch (props.getWhat.what) {
+            case 'all': fetchURL = 'http://localhost:3000/post/'; break;
+            case 'tag':
+                if (props.getWhat.tag === 'fur baby') fetchURL = 'http://localhost:3000/post/tag/:furbaby'
+                else if (props.getWhat.tag === 'scale baby') fetchURL = 'http://localhost:3000/post/tag/:scalebaby'
+                else if (props.getWhat.tag === 'exotic baby') fetchURL = 'http://localhost:3000/post/tag/:exoticbaby'
+                break;
+            case 'user': fetchURL = 'http://localhost:3000/posts/:user_id'; // !update w/variable
+        }
+        try {
+            fetch(fetchURL, {
+                method: "GET",
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                })
+            })
+                .then((res) => res.json())
+                .then((json) => {
+                    console.log(json);
+                    setPosts(json);
+                })
+        }
+        catch (error) { console.log(error) }
+
+        return posts.map((post, index) => {
+            return (
                 <div key={index} className='post'>
                     <img src={post.image} alt={post.title} />
                     <p>{post.title}</p>
@@ -13,12 +40,25 @@ const PostDisplay = (props) => {
             );
         });
     };
-    
-    return(
+
+    useEffect(() => {
+        postMapper();
+    }, [props.getWhat])
+
+    return (
         <div id='postDisplay'>
             <h5>Post Display</h5>
             <p>Multiple posts</p>
-            {postMapper()}
+            {posts.map((post, index) => {
+                return (
+                    <div key={index} className='post'>
+                        <img src={post.image} alt={post.title} />
+                        <p>{post.title}</p>
+                        <p>{post.description}</p>
+                        <p>{post.tag}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 };
