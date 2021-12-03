1cb6b532d40b01b21ef3d40da1d10499c5e86896
Gabrielle Ford <gabrielledeva@gmail.com>
2021-12-02 20:25:41 -0500

    added ability to update

--- src/components/TitleDescription.js
+++ src/components/TitleDescription.js
@@ -1,9 +1,17 @@
+import { Button } from "reactstrap";
+
+const deletePost = () => {
+  console.log("post deleted");
+};
+
 const TitleDescription = (props) => {
     return (
       <div>
-          <h3>Post</h3>
-          <p>{props.postTitle}</p>
-          <p>{props.postDescrip}</p>
+        <h3>Post</h3>
+        <p>{props.postTitle}</p>
+        <p>{props.postDescrip}</p>
+        <Button onClick={props.editActive}>{props.edit}</Button>
+        <Button onClick={deletePost}>Delete</Button>
       </div>
     );
 };
