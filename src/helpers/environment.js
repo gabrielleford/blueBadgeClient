5d92725803294e44274333e1800a0eb51f191d3d
Gabrielle Ford <gabrielledeva@gmail.com>
2021-12-08 19:53:02 -0500

    updated enviornment.js to reflect app rename

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1' :
         APIURL = 'http://localhost:3005';
         break;
-    case 'ksgf-instapet.herokuapp.com' :
+    case 'gfks-instapet-client.herokuapp.com' :
         APIURL = 'https://gfks-instapet-server.herokuapp.com'
 }
 
